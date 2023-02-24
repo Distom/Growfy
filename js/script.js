30d2425c9c242d7a71d5fdb142b120f33d0790e5
Dima Stekhov <dimastekhov@gmail.com>
2023-02-25 00:06:17 +0200

    turn ON menu buttons, fix AddBlock | rename files, css and html add theme saver to localeStorage add label to newsletter form

--- js/script.js
+++ js/script.js
@@ -8,10 +8,53 @@ let btnCollors = ['#5CC97B', '#FF6D35', '#3D96F4', '#FFCD1D'];
 let btnAnimationDuration = 300;
 
 addStars();
+updateTheme();
 
 document.addEventListener('click', toggleMenu);
 document.addEventListener('mouseover', changeBtnColor);
 document.addEventListener('click', toggleTheme);
+
+addScrollInto();
+window.onresize = function () {
+	addScrollInto();
+}
+
+function addScrollInto() {
+	let width = document.documentElement.clientWidth;
+	let position = width < 1200 ? 'start' : 'center';
+
+	let menuLinks = document.querySelectorAll('.header__menu-link');
+	menuLinks.forEach(link => {
+		link.onclick = function (event) {
+			let href = this.getAttribute('href');
+			if (!href || href == '#') return;
+
+			let target = document.querySelector(href);
+			if (!target) return;
+			target.scrollIntoView({ behavior: 'smooth', block: position });
+			event.preventDefault();
+		}
+	});
+}
+
+function updateTheme() {
+	let theme = localStorage.getItem('theme');
+	if (theme != 'lite-theme') return;
+
+	let toggler = document.querySelector('.header__theme-label');
+	toggler.classList.add('transition-after-off');
+	themeSwitcher.checked = true;
+	setTimeout(() => toggler.classList.remove('transition-after-off'), 0);
+
+	document.body.classList.remove('dark-theme');
+	document.body.classList.add('lite-theme');
+}
+
+function saveTheme() {
+	let isDark = document.body.classList.contains('dark-theme');
+	let theme = isDark ? 'dark-theme' : 'lite-theme';
+	localStorage.setItem('theme', theme);
+}
 
 function toggleTheme(e) {
 	if (e.target != themeSwitcher) return;
@@ -20,6 +63,7 @@ function toggleTheme(e) {
 	setTimeout(() => body.classList.remove('transition-all'), 300);
 	body.classList.toggle('dark-theme');
 	body.classList.toggle('lite-theme');
+	saveTheme();
 }
 
 function toggleMenu(e) {
@@ -105,7 +149,24 @@ function changeBtnColor(e) {
 	});
 }
 
-/* function changeBtnColor(e) {
+function randInt(min, max) {
+	return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+function addStars() {
+	let starsElems = document.querySelectorAll('.card-comment__stars');
+
+	starsElems.forEach(elem => {
+		let count = elem.dataset.stars;
+		for (let i = count; i > 0; i--) {
+			let star = document.createElement('span');
+			star.className = 'star';
+			elem.append(star);
+		}
+	});
+}
+
+/* function changeBtnColorTransition(e) {
 	let btn = e.target;
 	if (!btn) return;
 	if (!btn.classList.contains('button')) return;
@@ -132,20 +193,3 @@ function changeBtnColor(e) {
 		btn.removeEventListener('mouseout', mouseOut);
 	});
 } */
-
-function randInt(min, max) {
-	return Math.floor(min + Math.random() * (max + 1 - min));
-}
-
-function addStars() {
-	let starsElems = document.querySelectorAll('.card-comment__stars');
-
-	starsElems.forEach(elem => {
-		let count = elem.dataset.stars;
-		for (let i = count; i > 0; i--) {
-			let star = document.createElement('span');
-			star.className = 'star';
-			elem.append(star);
-		}
-	});
-}
